f0a7100e0befe308b4485ae66a1a6859eb8ddc47
Anish Sinha <sinha.anish008@gmail.com>
2026-08-20 14:48:05 +0530

    Create Q2.c

---
+++ Assignment4(20.08.26)/Q2.c
@@ -0,0 +1,15 @@
+/*
+A single Screen Cinema hall has one ticket counter . One customer arrives at a time .
+The requirements :
+1.Add Customers
+2.Serve Customers
+3.Show "Housefull" if the seats are full
+4.Display all the customers
+
+Sample input:
+Customer Ids:
+100
+115
+095
+086
+*/
