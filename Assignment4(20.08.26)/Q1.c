a5eeab57adc1a79857bf3c0c5260db0386e8f143
Anish Sinha <sinha.anish008@gmail.com>
2026-08-20 14:20:09 +0530

    Update Q1.c

--- Assignment4(20.08.26)/Q1.c
+++ Assignment4(20.08.26)/Q1.c
@@ -1,1 +1,9 @@
+/*Bank account keeping the transaction amount as it is processed by customer.
+THe transaction amount of last customer is stores at top position after completion of all transaction, the manager pull the amount one by one and display the amount and find
+total-transaction-amount to print define following funstion:
+push(): to insert amount in
+pop(): to remove amount
+dosum(): find sum of all transaction amount
+AMOUNT INSERTED IN FOLLOWING ORDER:
+1050,2090,3000,6000,5500,8900*/
 
